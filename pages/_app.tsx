1f534678caaa868fb32b87770a8f41d01305539d
Amine Elkhalidy <amineelkhalidy.a@gmail.com>
2022-12-03 21:58:25 +0100

    Adding new changes

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,30 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import React from "react";
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+
+// Head
+import Head from "next/head";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <React.Fragment>
+      {/* Head */}
+      <Head>
+        <meta
+          name="description"
+          content="This 4-page website will be a great test for your HTML & CSS skills. You'll be building out a marketing site, complete with custom form validation throughout."
+        />
+
+        <link
+          rel="shortcut icon"
+          href="assets/favicon.png"
+          type="image/x-icon"
+        />
+
+        <title>PayAPI - Landing page</title>
+      </Head>
+
+      <Component {...pageProps} />
+    </React.Fragment>
+  );
 }
