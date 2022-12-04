298ba2f5af8a49fdd2909dbaffd5b2006120889d
Amine Elkhalidy <amineelkhalidy.a@gmail.com>
2022-12-04 14:37:33 +0100

    Layout added

--- components/Layout/Layout.tsx
+++ components/Layout/Layout.tsx
@@ -1,11 +1,23 @@
 import React from "react";
 
+// Header & Footer
+import Header from "../Header/Header";
+import Footer from "../Footer/Footer";
+
 interface Child {
   children: React.ReactNode;
 }
 
 const Layout = ({ children }: Child) => {
-  return <div>{children}</div>;
+  return (
+    <React.Fragment>
+      <Header />
+
+      <main className="main">{children}</main>
+
+      <Footer />
+    </React.Fragment>
+  );
 };
 
 export default Layout;
